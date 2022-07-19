a9d433467e3eca8c74c1a9d9ec940d1e80d939bf
Joao Victor Negreiros <joaovictornsv@gmail.com>
2022-07-19 08:39:08 -0300

    feat: add prisma steps to build

--- scripts/shell/package.sh
+++ scripts/shell/package.sh
@@ -8,27 +8,34 @@ GREEN="\033[0;32m";
 GREEN_BOLD="\033[1;32m";
 RESET="\033[0m";
 
-# Zip directory to organize files before packing
+# ====== Zip directory to organize files before packing ======
 ZIP_DIRECTORY=.zip
 
-# Generate bundle files
+# ====== Generate bundle files ======
 yarn webpack;
 
-# Move files and organize folders to zip
+# ====== Move files and organize folders to zip ======
 node ./scripts/javascript/package.js;
 
-# Install necessary dependencies, then remove package.json and yarn.lock
+# ====== Install necessary dependencies, then remove package.json and yarn.lock ======
 yarn --cwd ${ZIP_DIRECTORY};
-rm ${ZIP_DIRECTORY}/package.json ${ZIP_DIRECTORY}/yarn.lock;
 
-# Generate zip file
+# ====== If the project use prisma, uncomment the lines below ======
+# cp "./schema.prisma" "${ZIP_DIRECTORY}/schema.prisma";
+
+# echo \
+# "DATABASE_URL=''
+# SHADOW_DATABASE_URL=''" >> "${ZIP_DIRECTORY}/.env";
+
+# yarn --cwd ${ZIP_DIRECTORY} add -D prisma@^3.15.2
+# yarn --cwd ${ZIP_DIRECTORY} prisma generate
+
+# ====== Generate zip file ======
 cd ${ZIP_DIRECTORY};
-PROJECT_NAME=$(node -p "require('../package.json').name");
-
 echo -e "${WHITE_BOLD}Packing...${RESET}"
-zip -q -r ../${PROJECT_NAME}.zip .;
+zip -q -r ../api.zip .;
 
-# Delete zip folder
+# ====== Delete zip folder ======
 cd ..;
 rm -rf ${ZIP_DIRECTORY};
 
@@ -45,4 +52,4 @@ echo -e "\n${GREEN}⠀⠀⠀⠀⣀⣤⡶⠞⠳⢶⣤⣀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠉⠛⠳⢾⡷⠞⠛⠉⠀⠀⠀⠀⠀${RESET}\n"
 
 echo -e "${GREEN_BOLD}Package complete!${RESET}";
-echo -e "${BLACK_BOLD}File:${WHITE} ${PROJECT_NAME}.zip${RESET}";
+echo -e "${BLACK_BOLD}File:${WHITE} api.zip${RESET}";
