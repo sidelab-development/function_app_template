efdf37c726838dfd63872aec83d25113641f0d58
Felipe Borges <felipejsborges@outlook.com>
2023-05-12 12:34:18 -0300

    hello world

--- src/app/handlers/functions/http-trigger/index.ts
+++ src/app/handlers/functions/http-trigger/index.ts
@@ -2,10 +2,18 @@ import { AzureFunction, Context, HttpRequest } from '@azure/functions';
 import { handleError, HTTP_STATUS_CODE } from 'sidelab-http-exceptions';
 import handleResponse from '../../utils/handle-response';
 
-const handler: AzureFunction = async (context: Context, req: HttpRequest): Promise<void> => {
-  const { id } = req.params;
+const handler: AzureFunction = async (
+  context: Context,
+  req: HttpRequest
+): Promise<void> => {
   try {
-    const response = { id };
+    // eslint-disable-next-line no-console
+    console.log(
+      'HTTP trigger function processed a request.',
+      req.method,
+      req.url
+    );
+    const response = { message: 'Hello world!' };
     context.res = handleResponse(response, HTTP_STATUS_CODE.GET);
   } catch (error) {
     context.res = handleError(error, context);
